ae566a383f77a1414dee16f53e8e9b6b761fc238
androiddisk <11062371+androiddisk@users.noreply.github.com>
2024-05-19 11:08:50 +0800

    Update build.assets.sh

--- build.assets.sh
+++ build.assets.sh
@@ -2,5 +2,9 @@ export GOPROXY=direct
 
 sudo apt-get update
 sudo apt-get install gcc-mingw-w64-i686 gcc-multilib
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
+
+tar -czvf linux_arm_v7_client.tar.gz npc conf/npc.conf conf/multi_account.conf
+
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
 tar -czvf linux_arm64_client.tar.gz npc conf/npc.conf conf/multi_account.conf
